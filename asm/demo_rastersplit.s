5d59f88134dfd2fa98df7458334a030faeb7c91f
laubzega <mileksmyk@gmail.com>
2020-09-19 20:15:56 -0700

    Add rastersplit example

--- asm/demo_rastersplit.s
+++ asm/demo_rastersplit.s
@@ -5,9 +5,16 @@
 ; https://github.com/madhackerslab/beamracer-examples
 ;
 ; More advanced implementation of rasterbars that relies on
-; VASYL self-programming to divide work between main CPU and VASYL,
-; thus achieving much better performance - total CPU time used is
-; approx. 10 rasterlines per frame.
+; VASYL programming to divide work between main CPU and VASYL,
+; thus achieving much better performance.
+;
+; Basically identical to demo_rasterbars.s sans the extra
+; code to handle right-hand side bar splits.
+;
+; Since VASYL code makes active use of both ports, it is critical
+; that the CPU accesses to port 0 do not overlap with VASYL's. Here it is
+; achieved by performing CPU's work in rasterlines 0 to 10. At that time VASYL
+; is doing nothing but WAIT-ing.
 
 BAR_COUNT  = 10
 LINE_COUNT = 234
@@ -39,13 +46,15 @@ sinptr2 = 203
         jsr copy_and_activate_dlist
  
 next_frame:
-        lda #100
+        lda #0
 wait_for_raster:
         cmp RASTER
         bne wait_for_raster
+        bit CTRL1
+        bmi wait_for_raster
 
-        ; Every frame, we calculate buffer addresses where to put the rasterbars.
-        ; This kind of calculations is best done by the CPU.
+        ; Every frame, we calculate buffer addresses where to put the rasterbar.
+        ; This kind of calculations is best made with the CPU.
         lda #<(line_ptrs - dl_start)
         sta VREG_ADR0
         lda #>(line_ptrs - dl_start)
@@ -55,7 +64,9 @@ wait_for_raster:
 
         ldx #BAR_COUNT - 1
 next_rasterbar:
-        stx B0C
+        stx $d020
+        lda #0
+        sta adr0_high
 
         ; The position corresponds to sin(8a+t)+sin(8a+3t),
         ; where "a" is rasterbar # and "t" is frame #.
@@ -79,7 +90,7 @@ partial_sum: lda #0 ; self modifying code
         clc
         adc #<(linecolors - dl_start)
         sta VREG_PORT0
-        lda #0
+        lda adr0_high
         adc #>(linecolors - dl_start)
         sta VREG_PORT0
         
@@ -93,14 +104,14 @@ partial_sum: lda #0 ; self modifying code
         adc #3
         sta sinptr2
 
-        lda #6
-        sta B0C
+        lda #0
+        sta $d020
  
         jmp next_frame
 
 multable:
         .repeat BAR_COUNT, LINE
-        .byte LINE * 8  ; Visual offset between bars.
+        .byte LINE * 8  ; visual offset between bars
         .endrep
 
 
@@ -108,7 +119,7 @@ multable:
 
 dlist:
 dl_start:
-        WAIT    14, 0   ; Wait for an off-screen location in both PAL and NTSC.
+        WAIT    14, 0      ; Wait for an off-screen location in both PAL and NTSC.
 
         ; In the first stage we are rendering the bars into a "linecolor" buffer
         ; that holds colors of individual rasterlines.
@@ -120,16 +131,16 @@ dl_start:
         MOV     VREG_ADR0,   <(line_ptrs_end - 1 - dl_start)
         MOV     VREG_ADR0+1, >(line_ptrs_end - 1 - dl_start)
         MOV     VREG_STEP0, -1
-        MOV     VREG_STEP1, 1
 
         ; Loop as many times as there are bars.
         SETA    BAR_COUNT - 1
+        MOV     VREG_STEP1, 1
 bar_loop:
         ; Initialize PORT1 pointer based on successive values from "line_ptrs" table.
         XFER    VREG_ADR1+1, (0)    ; hi-byte first - remember we're reading backwards.
         XFER    VREG_ADR1, (0)
 
-        ; Now write the rasterbar colors to the location set above.
+        ; Now write the rasterbar colors to location set above.
         MOV     VREG_PORT1, 0
         MOV     VREG_PORT1, 9
         MOV     VREG_PORT1, 2
@@ -152,26 +163,68 @@ bar_loop:
 
         ; Everything above was just a preparation. Now we repeatedly wait for
         ; the beginning of a line and then change the background color,
-        ; showing "linecolors" buffer on screen.
+        ; finally putting the bars on screen. Then we wait till the middle of
+        ; a rasterbar and change color again, this time walking the "line-color"
+        ; buffer from the end.
 
         WAIT    FIRST_LINE, 0   ; Starting line for rasterbar display.
 
+        ; PORT1 walks "line-color" buffer beginning-to-end.
         MOV     VREG_ADR1,   <(linecolors - dl_start)
         MOV     VREG_ADR1+1, >(linecolors - dl_start)
+        MOV     VREG_STEP1, 1
+        ; PORT0 walks it end-to-beginning.
+        MOV     VREG_ADR0,   <(linecolors_end - 1 - dl_start)
+        MOV     VREG_ADR0+1, >(linecolors_end - 1- dl_start)
+        MOV     VREG_STEP0, -1
 
         SETA    LINE_COUNT - 1
 line_loop:
-        ; We want to access each buffer location twice in quick succession:
-        ; 1. To read it.
-        ; 2. To clear it.
-        MOV     VREG_STEP1, 0   ; That's why we first set the step to 0, so that
-        XFER    $20, (1)        ; this read access does not advance port pointer.
-        MOV     VREG_STEP1, 1   ; Now we set it 1, so the pointer is increased
-        MOV     VREG_PORT1, 0   ; _after_ write access in this line.
+        ; Left-hand bars.
+        MOV     VREG_STEP1, 0
+        XFER    $20, (1)
 
-        DELAYV  1               ; Wait until next line begins.
+        ; Right-hand bars.
+        MASKV   0           ; Mask out vertical position bits.
+        WAIT    0, 35       ; Wait for cycle 35th.
 
-        DECA                    ; Iterate for all lines.
+        MOV     VREG_STEP0, 0
+        XFER    $20, (0)
+
+        ; Check if we have passed vertical midpoint of the screen.
+        ; If so, we can start clearing line-color table. Why here? Consider
+        ; which buffer locations of [A,B,C,D] buffer are needed to draw lines
+        ; 1, 2, 3 and 4. 3 is the first line where both locations are accessed
+        ; for the last time in a frame.
+        ;
+        ;   L      R
+        ;   ========
+        ; 1|A      D
+        ; 2|B      C
+        ;   --------
+        ; 3|C      B
+        ; 4|D      A
+
+        SKIP    ; SKIP modifier for the WAIT in the next line.
+        WAIT    FIRST_LINE + LINE_COUNT / 2, 0
+        BRA     skip       ; still upper half
+        MOV     VREG_PORT0, 0   ; clear color
+        MOV     VREG_PORT1, 0   ; clear color
+skip:
+        DELAYV  1   ; Wait until next line starts. Do it early, because
+                    ; in badlines there is not much cycle budget left.
+                    
+        ; Advance counters by reading from ports with appropriate step.
+        MOV     VREG_STEP0, -1  ; Going backwards.
+        XFER    VREG_STEP0, (0) ; We need a register to store the readout into.
+                                ; STEP0 will be reset anyway, so it's ok to trash it.
+                                ; We could also read into some unused color register,
+                                ; if STEP0 had to be preserved.
+
+        MOV     VREG_STEP1, 1   ; Going forwards.
+        XFER    VREG_STEP1, (1) ; As above, but with STEP1.
+
+        DECA
         BRA     line_loop
 
 dl_end_opcode:
@@ -182,11 +235,14 @@ line_ptrs:
         .word $ff00     ; Initial line pointers aim at a safe, distant location.
         .endrep
 line_ptrs_end:
+
 linecolors:
         .res LINE_COUNT, 0
+linecolors_end:
 dlend:
-        
+
 
 sinus:
     .include "sinus_ntsc2.inc"
 sinus_end:
+    .include "sinus_ntsc2.inc"
